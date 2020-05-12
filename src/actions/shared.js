9cb1cc2067826d21b2f4fcd67d5e7a608a7bcd89
Eugenio Pilastro Luca <507904+E-PL@users.noreply.github.com>
2020-05-13 00:34:12 +0200

    feat: Add the first actions: get tweets, users, authed user, and the thunk that will handle the initial data

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authuser'
+
+const AUTHED_ID = 'dan_abramov'
+
+export function handleInitialData() {
+    return getInitialData().then(
+        ({users, tweets}) => {
+            dispatch(receiveUsers(users))
+            dispatch(receiveTweets(tweets))
+            dispatch(setAuthedUser(AUTHED_ID))
+
+        }
+    )
+}
