5a1a8bac90bc646a7c3d589989367cd88ebd9b66
Eugenio Pilastro Luca <507904+E-PL@users.noreply.github.com>
2020-05-27 14:01:34 +0200

    feat: Add loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import authedUsers from './authedUsers'
 import tweets from './tweets'
 import users from './users'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
     authedUsers,
+    users,
     tweets,
-    users
+    loadingBar: loadingBarReducer,
 })
