2fc6642557b04f145b5ee338b6f4f45ffc215450
Eugenio Pilastro Luca <507904+E-PL@users.noreply.github.com>
2020-06-08 19:37:36 +0200

    feat: Add new tweet view

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import LoadingBar from 'react-redux-loading';
 import Dashboard from './Dashboard';
-
+import NewTweet from './NewTweet';
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
     return (
-      <div>{this.props.loading === true ? null : <Dashboard></Dashboard>}</div>
+      <div>
+        <LoadingBar />
+        {this.props.loading === true ? null : <NewTweet />}
+      </div>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 
-import authedUsers from './authedUsers';
+import authedUser from './authedUser';
 import tweets from './tweets';
 import users from './users';
-
+import { loadingBarReducer } from 'react-redux-loading';
 export default combineReducers({
-  authedUsers,
-  tweets,
+  authedUser,
   users,
+  tweets,
+  loadingBar: loadingBarReducer,
 });
